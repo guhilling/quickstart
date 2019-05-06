87e7292f799be703b1880b46adf708e7c2586618
Gunnar Hilling <gunnar@hilling.de>
2019-05-06 22:51:36 +0200

    configure servlet

--- jee-simple/src/main/java/de/hilling/jee/service/Ping.java
+++ jee-simple/src/main/java/de/hilling/jee/service/Ping.java
@@ -1,17 +1,30 @@
 package de.hilling.jee.service;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/ping")
+@WebServlet(value = "/ping",
+        initParams = {
+                @WebInitParam(name = "message", value = "hello, world!")
+        })
 public class Ping extends HttpServlet {
+
+    private String message;
+
+    @Override
+    public void init(ServletConfig config) {
+        message = config.getInitParameter("message");
+    }
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
-        response.getWriter().println("pong");
+        response.getWriter()
+                .println(message);
     }
 }

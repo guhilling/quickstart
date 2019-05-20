7537abeade3ed620a2ef5ba1020e88b278b7fb08
Gunnar Hilling <gunnar@hilling.de>
2019-05-20 22:34:00 +0200

    metrics, jwt

--- jira-creator/src/main/java/de/hilling/jee/jira/JiraServiceAdapter.java
+++ jira-creator/src/main/java/de/hilling/jee/jira/JiraServiceAdapter.java
@@ -5,6 +5,9 @@ import com.atlassian.jira.rest.client.api.domain.BasicIssue;
 import com.atlassian.jira.rest.client.api.domain.input.IssueInput;
 import com.atlassian.jira.rest.client.api.domain.input.IssueInputBuilder;
 import de.hilling.jee.jpa.ReceivedRequest;
+import org.eclipse.microprofile.metrics.Counter;
+import org.eclipse.microprofile.metrics.annotation.Metered;
+import org.eclipse.microprofile.metrics.annotation.Metric;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,12 +15,23 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 
 @RequestScoped
+@Metered(absolute = true)
 public class JiraServiceAdapter {
 
     private static final Logger LOG = LoggerFactory.getLogger(JiraServiceAdapter.class);
 
     private final JiraServerConfiguration serverConfiguration;
     private final JiraRestClient jiraRestClient;
+
+    @Inject
+    @Metric(description = "No of requested JIRA Issues",
+            displayName = "JIRARequestCount")
+    private Counter requestCount;
+
+    @Inject
+    @Metric(description = "No of errors when creating JIRA Issues",
+            displayName = "JIRAErrorCount")
+    private Counter jiraErrorCount;
 
     @Inject
     public JiraServiceAdapter(JiraServerConfiguration serverConfiguration, JiraRestClient jiraRestClient) {
@@ -30,6 +44,7 @@ public class JiraServiceAdapter {
     }
 
     public void createIssue(ReceivedRequest request) {
+        requestCount.inc();
         try {
             Long issueTypeId = serverConfiguration.issueIdForType(request.getType());
             final IssueInput issueInput = new IssueInputBuilder().setIssueTypeId(issueTypeId)
@@ -43,6 +58,7 @@ public class JiraServiceAdapter {
                                                    .claim();
             LOG.debug("created issue {}", issue);
         } catch (RuntimeException re) {
+            jiraErrorCount.inc();
             LOG.error("creating issue from request {} failed", request, re);
             throw re;
         }

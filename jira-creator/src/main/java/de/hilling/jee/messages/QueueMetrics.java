7b1b6a85310649983f74a681b751a22c97b12ee8
Gunnar Hilling <gunnar@hilling.de>
2019-05-22 19:35:08 +0200

    - fix test issue.

--- jira-creator/src/main/java/de/hilling/jee/messages/QueueMetrics.java
+++ jira-creator/src/main/java/de/hilling/jee/messages/QueueMetrics.java
@@ -1,12 +1,10 @@
 package de.hilling.jee.messages;
 
-import org.eclipse.microprofile.metrics.MetricRegistry;
 import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.annotation.Gauge;
 
 import javax.ejb.Startup;
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 import java.util.concurrent.atomic.AtomicLong;
 
 @ApplicationScoped
@@ -14,8 +12,6 @@ import java.util.concurrent.atomic.AtomicLong;
 public class QueueMetrics {
 
     private final AtomicLong queueLength = new AtomicLong(0);
-    @Inject
-    private MetricRegistry metrics;
 
     public void incQueueLength() {
         queueLength.incrementAndGet();

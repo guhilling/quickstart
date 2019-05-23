74438d880ab4ac908fa69acb6ce3237db5314853
Gunnar Hilling <gunnar@hilling.de>
2019-05-23 11:06:34 +0200

    - fix scope.

--- jira-creator/src/main/java/de/hilling/jee/jira/payment/Debit.java
+++ jira-creator/src/main/java/de/hilling/jee/jira/payment/Debit.java
@@ -1,8 +1,10 @@
 package de.hilling.jee.jira.payment;
 
+import javax.inject.Qualifier;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+@Qualifier
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Debit {
 }
